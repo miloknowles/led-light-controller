9be1e2ab2b1fc2c4101e6388a099783d2c0830ac
Milo Knowles <mknowles@mit.edu>
2016-06-07 21:33:27 -0700

    Initial Commit

---
+++ LED Light Control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LED Light Control
+//
+//  Created by Milo Knowles on 6/7/16.
+//  Copyright © 2016 Grassy Knoll Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
